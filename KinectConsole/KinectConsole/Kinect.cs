d4b4dd96fb3dd18bc4f55a2c4b45834fefe16436
Bruno Costa <doravante2@gmail.com>
2013-01-22 09:37:01 -0200

    Refactoring classes. Adding classes for Arduino control.

--- KinectConsole/KinectConsole/Kinect.cs
+++ KinectConsole/KinectConsole/Kinect.cs
@@ -2,28 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO.Ports;
 using System.Threading.Tasks;
 using Microsoft.Kinect;
 
 namespace KinectConsole
 {
-    class Program
+    class Kinect
     {
         KinectSensor kinect = null;
         Skeleton[] skeletonData = null;
 
-        static void Main(string[] args)
+        public Kinect()
         {
-            Program p = new Program();
-            p.StartKinectST();
-
-            while (true)
-            {
-                System.Threading.Thread.Sleep(50000);
-            }
+            startKinectSensor();
         }
 
-        void StartKinectST()
+        public void startKinectSensor()
         {
             kinect = KinectSensor.KinectSensors.FirstOrDefault(s => s.Status == KinectStatus.Connected); // Get first Kinect Sensor
             kinect.SkeletonStream.Enable(); // Enable skeletal tracking
@@ -44,27 +39,70 @@ namespace KinectConsole
                     skeletonFrame.CopySkeletonDataTo(this.skeletonData); // get the skeletal information in this frame
                 }
 
-                DrawSkeletons();
+                drawSkeletons();
             }
         }
 
-        private void DrawSkeletons()
+        public void drawSkeletons()
         {
             foreach (Skeleton skeleton in this.skeletonData)
             {
                 if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
                 {
-                    DrawTrackedSkeletonJoints(skeleton.Joints);
+                    drawTrackedSkeletonJoints(skeleton.Joints);
                 }
             }
         }
 
-        private void DrawTrackedSkeletonJoints(JointCollection jointCollection)
+        private void drawTrackedSkeletonJoints(JointCollection jointCollection)
         {
             Console.WriteLine("Elbow right: <" + jointCollection[JointType.ElbowRight].Position.X + ", " + jointCollection[JointType.ElbowRight].Position.Y + ">");
             Console.WriteLine("Elbow left: <" + jointCollection[JointType.ElbowLeft].Position.X + ", " + jointCollection[JointType.ElbowLeft].Position.Y + ">");
             Console.WriteLine("Knee right: <" + jointCollection[JointType.KneeRight].Position.X + ", " + jointCollection[JointType.KneeRight].Position.Y + ">");
             Console.WriteLine("Knee left: <" + jointCollection[JointType.KneeLeft].Position.X + ", " + jointCollection[JointType.KneeLeft].Position.Y + ">");
+        }
+    }
+
+    class Arduino
+    {
+        SerialPort port = null;
+
+        public Arduino()
+        {
+            port = new SerialPort("COM3", 9600);
+            port.Open();           
+        }
+
+        private void establishContact()
+        {
+            port.ReadLine();
+            port.Write("A");
+        }
+
+        public void turnLedOn()
+        {
+            port.Write("O");
+        }
+
+        public void turnLedOff()
+        {
+            port.Write("F");
+        }
+    }
+
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            Arduino arduino = new Arduino();
+
+            while (true)
+            {
+                arduino.turnLedOff();
+                System.Threading.Thread.Sleep(1000);
+                arduino.turnLedOn();
+                System.Threading.Thread.Sleep(1000);
+            }
         }
     }
 }
