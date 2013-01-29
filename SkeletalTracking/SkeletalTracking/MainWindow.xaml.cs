7b0c4267b632ba24ff350e0f3789bbd26e709b7c
Bruno Costa <doravante2@gmail.com>
2013-01-29 11:30:21 -0200

    Project for rendering skeleton on screen

---
+++ SkeletalTracking/SkeletalTracking/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿/////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// This code is licensed under the terms of the Microsoft Kinect for
+// Windows SDK (Beta) License Agreement:
+// http://kinectforwindows.org/KinectSDK-ToU
+//
+// Modified on 01/29/2013
+/////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace SkeletalTracking
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        KinectSensor kinect;
+        Skeleton[] skeletonData = null;
+        Canvas canvas = null;
+        const double mult = -100;
+        const double deslocamento = 50;
+        const double raio = 8;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetupKinect();
+        }
+
+        private void SetupKinect()
+        {
+            kinect = KinectSensor.KinectSensors.FirstOrDefault(s => s.Status == KinectStatus.Connected); // Get first Kinect Sensor
+            kinect.SkeletonStream.Enable(); // Enable skeletal tracking
+
+            skeletonData = new Skeleton[kinect.SkeletonStream.FrameSkeletonArrayLength]; // Allocate ST data
+
+            kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(Kinect_SkeletonFrameReady); // Get Ready for Skeleton Ready Events
+
+            kinect.Start(); // Start Kinect sensor
+
+            canvas = (Canvas)FindName("MainCanvas");
+        }
+
+        void Kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame()) // Open the Skeleton frame
+            {
+                if (skeletonFrame != null && this.skeletonData != null) // check that a frame is available
+                {
+                    skeletonFrame.CopySkeletonDataTo(this.skeletonData); // get the skeletal information in this frame
+                }
+
+                canvas.Children.Clear();
+                DrawSkeletons();
+            }
+        }
+
+        private void DrawSkeletons()
+        {
+            foreach (Skeleton skeleton in this.skeletonData)
+            {
+                if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                {
+                    DrawTrackedSkeletonJoints(skeleton.Joints);
+                }
+            }
+        }
+
+        private void DrawTrackedSkeletonJoints(JointCollection jointCollection)
+        {
+            // Render Head and Shoulders
+            DrawBone(jointCollection[JointType.Head], jointCollection[JointType.ShoulderCenter]);
+            DrawBone(jointCollection[JointType.ShoulderCenter], jointCollection[JointType.ShoulderLeft]);
+            DrawBone(jointCollection[JointType.ShoulderCenter], jointCollection[JointType.ShoulderRight]);
+
+            // Render Left Arm
+            DrawBone(jointCollection[JointType.ShoulderLeft], jointCollection[JointType.ElbowLeft]);
+            DrawBone(jointCollection[JointType.ElbowLeft], jointCollection[JointType.WristLeft]);
+            DrawBone(jointCollection[JointType.WristLeft], jointCollection[JointType.HandLeft]);
+
+            // Render Right Arm
+            DrawBone(jointCollection[JointType.ShoulderRight], jointCollection[JointType.ElbowRight]);
+            DrawBone(jointCollection[JointType.ElbowRight], jointCollection[JointType.WristRight]);
+            DrawBone(jointCollection[JointType.WristRight], jointCollection[JointType.HandRight]);
+
+            // Render other bones...
+        }
+
+        private void DrawBone(Joint jointFrom, Joint jointTo)
+        {
+            if (jointFrom.TrackingState == JointTrackingState.NotTracked ||
+            jointTo.TrackingState == JointTrackingState.NotTracked)
+            {
+                return; // nothing to draw, one of the joints is not tracked
+            }
+
+            if (jointFrom.TrackingState == JointTrackingState.Inferred ||
+            jointTo.TrackingState == JointTrackingState.Inferred)
+            {
+                //DrawNonTrackedBoneLine(jointFrom.Position, jointTo.Position);  // Draw thin lines if either one of the joints is inferred
+            }
+
+            if (jointFrom.TrackingState == JointTrackingState.Tracked &&
+            jointTo.TrackingState == JointTrackingState.Tracked)
+            {
+                DrawTrackedBoneLine(jointFrom.Position, jointTo.Position);  // Draw bold lines if the joints are both tracked
+            }
+        }
+
+        private void DrawTrackedBoneLine(SkeletonPoint from, SkeletonPoint to)
+        {
+            DrawJoint(from);
+            DrawJoint(to);
+
+            Line line = new Line();
+
+            line.X1 = FixPointPosition(from.X);
+            line.Y1 = FixPointPosition(from.Y);
+            line.X2 = FixPointPosition(to.X);
+            line.Y2 = FixPointPosition(to.Y);
+
+            line.StrokeThickness = 2;
+            line.Stroke = System.Windows.Media.Brushes.Black;
+            line.SnapsToDevicePixels = true;
+            line.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
+            
+            canvas.Children.Add(line);
+        }
+
+        private void DrawJoint(SkeletonPoint joint)
+        {
+            Ellipse e = new Ellipse();
+            e.Width = raio;
+            e.Height = raio;
+            e.Fill = System.Windows.Media.Brushes.Blue;
+            e.Stroke = System.Windows.Media.Brushes.Black;
+            e.StrokeThickness = 1;
+
+            e.Margin = new Thickness(FixPointPosition(joint.X) - raio / 2, FixPointPosition(joint.Y) - raio / 2, 0, 0);
+            canvas.Children.Add(e);
+        }
+
+        private double FixPointPosition(double p)
+        {
+            return p * mult + deslocamento;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            //Cleanup
+        }
+    }
+}
